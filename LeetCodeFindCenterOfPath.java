013debf84e1ade2b05a6ec2edb8a6007f2af3b9e
Somesh Gangwar <39381816+Somesh1997@users.noreply.github.com>
2021-06-21 23:34:29 +0530

    Update LeetCodeFindCenterOfPath.java
    O(1) also updated

--- LeetCodeFindCenterOfPath.java
+++ LeetCodeFindCenterOfPath.java
@@ -23,3 +23,11 @@ class Solution {
       return -1;
     }
 }
+
+/*
+    
+    O(1) Solution
+public int findCenter(int[][] e) {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+}
+  */
