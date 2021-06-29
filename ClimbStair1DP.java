639f22507fe482bd5a2297683ceb40d23e5b8c62
Somesh Gangwar <39381816+Somesh1997@users.noreply.github.com>
2021-06-29 17:34:14 +0530

    Create ClimbStair1DP.java
    Climb stair completed

---
+++ ClimbStair1DP.java
@@ -0,0 +1,31 @@
+package dp;
+
+import java.util.Scanner;
+
+public class Climb {
+
+	public Climb() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static int climbStair(int n) {
+		if (n == 0) {
+			return 1;
+
+		}
+		int count = 0;
+		for (int i = 1; i <= 3; i++) {
+			if (n - i >= 0) {
+				count = count + climbStair(n - i);
+			}
+		}
+		return count;
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(climbStair(sc.nextInt()));
+	}
+
+}
